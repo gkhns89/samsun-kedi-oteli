0781f6be124f921210f1744febb1146ef832634a
Gökhan <gokhansisman@outlook.com>
2025-01-06 04:42:12 +0300

    Header design and fixes

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,23 +1,33 @@
-import React from 'react'
+import React from "react";
 
 function header() {
   return (
-    <header>
-        <div aria-label='header-content' className='w-full h-32 bg-gray-400 flex flex-row items-center justify-between px-12'>
-            <div aria-label='header-logo' className='w-48 h-full bg-red-300'>
-                <img src='./images/logo.png' alt='logo' className='h-full w-full object-contain' />
-            </div>
-            <nav aria-label='header-nav' className='min-w-64 h-full bg-green-300'>
-            <ul className='w-full h-full flex flex-row items-center justify-between'>
-                <li>Hakkımızda</li>
-                <li>Hizmetlerimiz</li>
-                <li>S.S.S.</li>
-                <li>İletişim</li>
-            </ul>
-            </nav>
+    <header className="bg-header-color">
+      <div
+        aria-label="header-content"
+        className="w-full h-32 flex flex-row items-center justify-between px-12"
+      >
+        <div aria-label="header-logo" className="min-w-48 h-full">
+          <img
+            src="./images/logo-1.png"
+            alt="logo"
+            className="h-full w-full object-contain"
+          />
         </div>
+        <nav aria-label="header-nav" className="w-full h-full  text-txt-2">
+          <ul className="w-full h-full flex flex-row items-center justify-end space-x-10">
+            <li>Hakkımızda</li>
+            <li>Hizmetlerimiz</li>
+            <li>S.S.S.</li>
+            <li>İletişim</li>
+            <li>
+              <button className="btn-primary">Randevu Al</button>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
-  )
+  );
 }
 
-export default header
+export default header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['"Poppins"', ...defaultTheme.fontFamily.sans],
+        sans: ['"Poppins"',  ...defaultTheme.fontFamily.sans],
+        'button': ['Chewy', ...defaultTheme.fontFamily.sans],
+        },
+      colors: {
+        "btn-color-1": "#E4813A",
+        "btn-color-1-hover": "#c76a2f",
+        "btn-color-1-focus": "#c76a2f",
+"header-color": "#F2F2F2",
+        "btn-color-2": "#FBE2AC",
+        "btn-color-2-hover": "#f8d3a0",
+        "btn-color-2-focus": "#f8d3a0",
+
+        "txt-1": "#F2F2F2",
+        "txt-2": "#3C3C3C",
       },
-    }
-  }
-}
+    },
+  },
+};
